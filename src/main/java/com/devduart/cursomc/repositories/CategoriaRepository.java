80ad0f1f8a6f316ab39b10352b9540e74e878d8e
Dev Duart <walyssonduarte@gmail.com>
2021-10-11 01:10:09 -0300

    Create Repository and Service for Categoria

---
+++ src/main/java/com/devduart/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.devduart.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devduart.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/devduart/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.devduart.cursomc.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devduart.cursomc.domain.Categoria;
+import com.devduart.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
