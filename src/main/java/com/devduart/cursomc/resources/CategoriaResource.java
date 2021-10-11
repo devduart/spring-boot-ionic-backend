9aacef3b9a64890621d187ae592f47aea6914e10
Dev Duart <walyssonduarte@gmail.com>
2021-10-11 00:09:06 -0300

    Test First Domain Class

--- src/main/java/com/devduart/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/devduart/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.devduart.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devduart.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/Categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está Funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escitorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
