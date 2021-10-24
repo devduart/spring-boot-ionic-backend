18e62527358dae48b4252e48706da161f8b512ea
Dev Duart <walyssonduarte@gmail.com>
2021-10-23 23:13:51 -0300

    Ajustes Paginação, Erros retorno da Validaçao

--- src/main/java/com/devduart/cursomc/services/CategoriaService.java
+++ src/main/java/com/devduart/cursomc/services/CategoriaService.java
@@ -1,12 +1,19 @@
 package com.devduart.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.devduart.cursomc.domain.Categoria;
+import com.devduart.cursomc.dto.CategoriaDTO;
 import com.devduart.cursomc.repositories.CategoriaRepository;
+import com.devduart.cursomc.services.exceptions.DataIntegrityException;
 import com.devduart.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +26,39 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objet nao encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	//paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
